3ca57a10cd31dfbb530dcc273945ecf8b7df2beb
Cyberarm <matthewlikesrobots@gmail.com>
2025-06-28 22:53:29 -0500

    Removed code that was moved to hierarchy model class, stubbed gui layout, ran formatter on main.cpp

--- main.cpp
+++ main.cpp
@@ -22,139 +22,18 @@
 #define GLTF_FILENAME "D:/W3DHub/games/tiberian-sun-reborn/LevelEdit/TS_Level/ts_level.glb"
 #define W3D_FILENAME "D:/W3DHub/games/tiberian-sun-reborn/LevelEdit/TS_Level/ts_level.w3d"
 
-bool exportW3DHierarchyModel(tinygltf::Model &model, const std::string& filename, bool optimize_for_terrain = false)
-{
-    // TODO: Translate GLTF data to W3D data and write it out.
-    SDL_IOStream* stream = SDL_IOFromFile(filename.c_str(), "wb");
+bool exportW3DHierarchyModel(tinygltf::Model &model, const std::string &filename, bool optimize_for_terrain = false) {
+    SDL_IOStream *stream = SDL_IOFromFile(filename.c_str(), "wb");
 
     auto m_writer = ChunkSaveClass(stream);
 
     W3dHierarchyModel(model, m_writer, true);
 
     SDL_CloseIO(stream);
-    return  true;
-
-    std::vector<W3dPivotStruct> pivots = {};
-    W3dPivotStruct pivot = {};
-    uint32_t pivot_parent_idx = 0xffffffff;
-
-    // write RootTransform
-    strcpy(pivot.Name, "RootTransform");
-    pivot.ParentIdx = pivot_parent_idx;
-    pivot.Translation.X = 0;
-    pivot.Translation.Y = 0;
-    pivot.Translation.Z = 0;
-    pivot.EulerAngles.X = 0;
-    pivot.EulerAngles.Y = 0;
-    pivot.EulerAngles.Z = 1;
-    pivot.Rotation.Q[0] = 0;
-    pivot.Rotation.Q[1] = 0;
-    pivot.Rotation.Q[2] = 0;
-    pivot.Rotation.Q[3] = 1;
-
-    pivots.emplace_back(pivot);
-
-
-    for (auto node : model.nodes)
-    {
-        if (node.mesh < 0)
-            continue;
-
-        tinygltf::Mesh mesh = model.meshes.at(node.mesh);
-
-        // Node's mesh is not a proxy/placeholder/pivot thingy
-        if (mesh.name.find('~') == std::string::npos)
-            continue;
-
-        strcpy(pivot.Name, mesh.name.c_str());
-        pivot.ParentIdx = 0;
-        pivot.Translation.X = node.translation.size() > 0 ? node.translation[0] : 0;
-        pivot.Translation.Y = node.translation.size() > 0 ? node.translation[1] : 0;
-        pivot.Translation.Z = node.translation.size() > 0 ? node.translation[2] : 0;
-        pivot.EulerAngles.X = node.rotation.size() > 0 ? node.rotation[0] : 0;
-        pivot.EulerAngles.Y = node.rotation.size() > 0 ? node.rotation[1] : 0;
-        pivot.EulerAngles.Z = node.rotation.size() > 0 ? node.rotation[2] : 0;
-        pivot.Rotation.Q[0] = 0;
-        pivot.Rotation.Q[1] = 0;
-        pivot.Rotation.Q[2] = 0;
-        pivot.Rotation.Q[3] = 1;
-
-        pivots.emplace_back(pivot);
-    }
-
-    auto writer = ChunkSaveClass(stream);
-    writer.begin_chunk(W3D_CHUNK_HIERARCHY);
-    W3dHierarchyStruct hierarchy_header = {};
-    writer.begin_chunk(W3D_CHUNK_HIERARCHY_HEADER);
-    hierarchy_header.Version = W3D_CURRENT_HTREE_VERSION;
-    strcpy(hierarchy_header.Name, "ts_level");
-    hierarchy_header.NumPivots = pivots.size();
-    W3dVectorStruct v = {0, 0, 0};
-    hierarchy_header.Center = v;
-    writer.write((uint8_t *) &hierarchy_header, sizeof(W3dHierarchyStruct));
-    writer.end_chunk();
-
-    // WRITE PIVOTS!
-    writer.begin_chunk(W3D_CHUNK_PIVOTS);
-    for(auto piv : pivots)
-    {
-        writer.write((uint8_t *) &piv, sizeof(W3dPivotStruct));
-    }
-    writer.end_chunk();
-    writer.end_chunk();
-
-
-    W3dHLodHeaderStruct hLodHeaderStruct = {};
-    W3dHLodArrayHeaderStruct hLodArrayHeaderStruct = {};
-    writer.begin_chunk(W3D_CHUNK_HLOD);
-    writer.begin_chunk(W3D_CHUNK_HLOD_HEADER);
-    hLodHeaderStruct.Version = W3D_CURRENT_HLOD_VERSION;
-    strcpy(hLodHeaderStruct.Name, "ts_level");
-    strcpy(hLodHeaderStruct.HierarchyName, "ts_level");
-    hLodHeaderStruct.LodCount = 1;
-    writer.write((uint8_t *) &hLodHeaderStruct, sizeof(W3dHLodHeaderStruct));
-    writer.end_chunk();
-    writer.begin_chunk(W3D_CHUNK_HLOD_PROXY_ARRAY);
-    writer.begin_chunk(W3D_CHUNK_HLOD_SUB_OBJECT_ARRAY_HEADER);
-    hLodArrayHeaderStruct.ModelCount = pivots.size();
-    hLodArrayHeaderStruct.MaxScreenSize = 0;
-    writer.write((uint8_t *) &hLodArrayHeaderStruct, sizeof(W3dHLodArrayHeaderStruct));
-    writer.end_chunk();
-    W3dHLodSubObjectStruct w3DHLodSubObjectStruct = {};
-    size_t i = 0;
-    for(auto piv : pivots)
-    {
-        // Skip root transform
-        if (i == 0)
-        {
-            i++;
-            continue;
-        }
-
-        writer.begin_chunk(W3D_CHUNK_HLOD_SUB_OBJECT);
-        std::string name = piv.Name;
-        size_t tilde_index = name.find('~');
-        printf("tilde: %zu (%s)\n", tilde_index, name.c_str());
-        if (tilde_index)
-        {
-            name = name.substr(0, tilde_index);
-        }
-        strcpy(w3DHLodSubObjectStruct.Name, name.c_str());
-        w3DHLodSubObjectStruct.BoneIndex = i;
-        writer.write((uint8_t *) &w3DHLodSubObjectStruct, sizeof(W3dHLodSubObjectStruct));
-        writer.end_chunk();
-
-        i++;
-    }
-    writer.end_chunk();
-    writer.end_chunk();
-
-    SDL_CloseIO(stream);
-
     return true;
 }
 
-bool loadModel(tinygltf::Model &model, const std::string& filename) {
+bool loadModel(tinygltf::Model &model, const std::string &filename) {
     tinygltf::TinyGLTF loader;
     std::string err;
     std::string warn;
@@ -180,11 +59,10 @@ bool loadModel(tinygltf::Model &model, const std::string& filename) {
     return result;
 }
 
-int main(int, char**)
-{
+int main(int, char **) {
     tinygltf::Model model;
     if (loadModel(model, GLTF_FILENAME)) {
-        for (const auto& mesh: model.meshes) {
+        for (const auto &mesh: model.meshes) {
             std::cout << "    Mesh: " << mesh.name << std::endl;
         }
         for (auto node: model.nodes) {
@@ -206,8 +84,7 @@ int main(int, char**)
 
     // Setup SDL
     // [If using SDL_MAIN_USE_CALLBACKS: all code below until the main loop starts would likely be your SDL_AppInit() function]
-    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
-    {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
         printf("Error: SDL_Init(): %s\n", SDL_GetError());
         return -1;
     }
@@ -215,9 +92,8 @@ int main(int, char**)
     // Create SDL window graphics context
     float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
     SDL_WindowFlags window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
-    SDL_Window* window = SDL_CreateWindow(APP_TITLE, (int)(1280 * main_scale), (int)(720 * main_scale), window_flags);
-    if (window == nullptr)
-    {
+    SDL_Window *window = SDL_CreateWindow(APP_TITLE, (int) (1280 * main_scale), (int) (720 * main_scale), window_flags);
+    if (window == nullptr) {
         printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
         return -1;
     }
@@ -225,16 +101,15 @@ int main(int, char**)
     SDL_ShowWindow(window);
 
     // Create GPU Device
-    SDL_GPUDevice* gpu_device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_DXIL | SDL_GPU_SHADERFORMAT_METALLIB,true,nullptr);
-    if (gpu_device == nullptr)
-    {
+    SDL_GPUDevice *gpu_device = SDL_CreateGPUDevice(
+            SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_DXIL | SDL_GPU_SHADERFORMAT_METALLIB, true, nullptr);
+    if (gpu_device == nullptr) {
         printf("Error: SDL_CreateGPUDevice(): %s\n", SDL_GetError());
         return -1;
     }
 
     // Claim window for GPU Device
-    if (!SDL_ClaimWindowForGPUDevice(gpu_device, window))
-    {
+    if (!SDL_ClaimWindowForGPUDevice(gpu_device, window)) {
         printf("Error: SDL_ClaimWindowForGPUDevice(): %s\n", SDL_GetError());
         return -1;
     }
@@ -243,17 +118,20 @@ int main(int, char**)
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO &io = ImGui::GetIO();
+    (void) io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.Fonts->AddFontFromFileTTF("C:/Windows/Fonts/Arial.ttf");
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
     //ImGui::StyleColorsLight();
 
     // Setup scaling
-    ImGuiStyle& style = ImGui::GetStyle();
-    style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.ScaleAllSizes(
+            main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
     style.FontScaleDpi = main_scale;        // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
 
     // Setup Platform/Renderer backends
@@ -287,8 +165,7 @@ int main(int, char**)
 
     // Main loop
     bool done = false;
-    while (!done)
-    {
+    while (!done) {
         // Poll and handle events (inputs, window resize, etc.)
         // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
         // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
@@ -296,8 +173,7 @@ int main(int, char**)
         // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         // [If using SDL_MAIN_USE_CALLBACKS: call ImGui_ImplSDL3_ProcessEvent() from your SDL_AppEvent() function]
         SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
+        while (SDL_PollEvent(&event)) {
             ImGui_ImplSDL3_ProcessEvent(&event);
             if (event.type == SDL_EVENT_QUIT)
                 done = true;
@@ -308,8 +184,7 @@ int main(int, char**)
         SDL_Delay(16);
 
         // [If using SDL_MAIN_USE_CALLBACKS: all code below would likely be your SDL_AppIterate() function]
-        if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED)
-        {
+        if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
             continue;
         }
 
@@ -318,68 +193,76 @@ int main(int, char**)
         ImGui_ImplSDL3_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (!show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
             static float f = 0.0f;
             static int counter = 0;
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit4("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            ImGui::SetNextWindowSize(io.DisplaySize);
+            ImGui::SetNextWindowPos({0, 0});
+            ImGui::Begin("Hello, world!", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove |
+                                                   ImGuiWindowFlags_NoSavedSettings);                          // Create a window called "Hello, world!" and append into it.
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
+            ImGui::BeginTable("TABLE", 2, ImGuiTableFlags_BordersInnerV, {0,
+                                                                          io.DisplaySize.y}); // FIXME: Height doesn't compensate for outer elements/window padding
+            ImGui::TableNextColumn();
+            ImGui::Text("GLTF / GLB");
+            ImGui::Text("DRAG and DROP .gltf or .glb file here");
+            ImGui::Text("NOTE: Embedded images will not be used.");
+            ImGui::Spacing();
+            ImGui::Text("\nTODO: Show TreeView of GLTF contents here once loaded.");
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
+            ImGui::TableNextColumn();
+            ImGui::Text("W3D Export Options");
+            static char buf[16] = "";
+            ImGui::InputTextWithHint("", "Leave blank if unsure", buf, 16);
+            ImGui::SameLine();
+            ImGui::Text("Container Name");
+            const char *listbox_items[] = {"Hierarchy Model", "Mesh"};
+            static int listbox_item_current = 0;
+            ImGui::Combo("W3D Type", &listbox_item_current, listbox_items, 2);
+            static bool opt_terrain = false;
+            ImGui::Checkbox("Optimize for Terrain", &opt_terrain);
+
+            // Export Button
+            // NOTE: We manually move imgui's 'cursor' so it MUST be the last element of the 'window' created
+            //       to ensure proper auto element placement.
+            float button_width = std::max(ImGui::CalcTextSize("Export").x + style.FramePadding.x * 2.f, 128.f);
+            float button_height = ImGui::CalcTextSize("Export").y + style.FramePadding.y * 2.f;
+            ImGui::SetCursorPosY(ImGui::GetCursorPosY() + ImGui::GetContentRegionAvail().y - button_height);
+            ImGui::ProgressBar(0.25f, {ImGui::GetContentRegionAvail().x - (button_width + style.ItemSpacing.x), 0});
+            ImGui::SameLine();
+            ImGui::Button("Export", {button_width, 0});
+            ImGui::EndTable();
+
             ImGui::End();
         }
 
         // Rendering
         ImGui::Render();
-        ImDrawData* draw_data = ImGui::GetDrawData();
+        ImDrawData *draw_data = ImGui::GetDrawData();
         const bool is_minimized = (draw_data->DisplaySize.x <= 0.0f || draw_data->DisplaySize.y <= 0.0f);
 
-        SDL_GPUCommandBuffer* command_buffer = SDL_AcquireGPUCommandBuffer(gpu_device); // Acquire a GPU command buffer
+        SDL_GPUCommandBuffer *command_buffer = SDL_AcquireGPUCommandBuffer(gpu_device); // Acquire a GPU command buffer
 
-        SDL_GPUTexture* swapchain_texture;
-        SDL_AcquireGPUSwapchainTexture(command_buffer, window, &swapchain_texture, nullptr, nullptr); // Acquire a swapchain texture
+        SDL_GPUTexture *swapchain_texture;
+        SDL_AcquireGPUSwapchainTexture(command_buffer, window, &swapchain_texture, nullptr,
+                                       nullptr); // Acquire a swapchain texture
 
-        if (swapchain_texture != nullptr && !is_minimized)
-        {
+        if (swapchain_texture != nullptr && !is_minimized) {
             // This is mandatory: call ImGui_ImplSDLGPU3_PrepareDrawData() to upload the vertex/index buffer!
             ImGui_ImplSDLGPU3_PrepareDrawData(draw_data, command_buffer);
 
             // Setup and start a render pass
             SDL_GPUColorTargetInfo target_info = {};
             target_info.texture = swapchain_texture;
-            target_info.clear_color = SDL_FColor { clear_color.x, clear_color.y, clear_color.z, clear_color.w };
+            target_info.clear_color = SDL_FColor{clear_color.x, clear_color.y, clear_color.z, clear_color.w};
             target_info.load_op = SDL_GPU_LOADOP_CLEAR;
             target_info.store_op = SDL_GPU_STOREOP_STORE;
             target_info.mip_level = 0;
             target_info.layer_or_depth_plane = 0;
             target_info.cycle = false;
-            SDL_GPURenderPass* render_pass = SDL_BeginGPURenderPass(command_buffer, &target_info, 1, nullptr);
+            SDL_GPURenderPass *render_pass = SDL_BeginGPURenderPass(command_buffer, &target_info, 1, nullptr);
 
             // Render ImGui
             ImGui_ImplSDLGPU3_RenderDrawData(draw_data, command_buffer, render_pass);
